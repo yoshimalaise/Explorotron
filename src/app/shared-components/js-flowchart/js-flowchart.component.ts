e76947f38cbba0f973f7b7b0c6b4b8ddcbc2810f
yoshimalaise <yoshimalaise@users.noreply.github.com>
2023-04-19 19:51:20 +0200

    added to update chart ifother file is selected

--- src/app/shared-components/js-flowchart/js-flowchart.component.ts
+++ src/app/shared-components/js-flowchart/js-flowchart.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, Input, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, Input, OnInit, SimpleChanges, ViewChild } from '@angular/core';
 import {convertCodeToSvg, createSVGRender, convertCodeToFlowTree } from 'js2flowchart';
 import { defaultTheme } from './js-flowchart.theme';
 
@@ -14,11 +14,19 @@ export class JsFlowchartComponent implements AfterViewInit {
   constructor() { }
 
   ngAfterViewInit(): void {
-      const flowTree = convertCodeToFlowTree(this.snippet);
-      const svgRender = createSVGRender();
-      svgRender.applyTheme(defaultTheme);
-      const svg = svgRender.buildShapesTree(flowTree).print();
-      this.chartContainer.nativeElement.innerHTML = svg;
+      this.updateChart(this.snippet);
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    this.updateChart(changes.snippet.currentValue);
+  }
+
+  updateChart(code: string) {
+    const flowTree = convertCodeToFlowTree(this.snippet);
+    const svgRender = createSVGRender();
+    svgRender.applyTheme(defaultTheme);
+    const svg = svgRender.buildShapesTree(flowTree).print();
+    this.chartContainer.nativeElement.innerHTML = svg;
   }
 
 }
