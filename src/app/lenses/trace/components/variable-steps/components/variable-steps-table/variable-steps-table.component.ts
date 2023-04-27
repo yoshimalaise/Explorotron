83ddb12250aeaa5e20afe4f67f720112e9e0c4a5
yoshimalaise <yoshimalaise@users.noreply.github.com>
2023-04-27 12:00:47 +0200

    wip integrating trace tables

--- src/app/lenses/trace/components/variable-steps/components/variable-steps-table/variable-steps-table.component.ts
+++ src/app/lenses/trace/components/variable-steps/components/variable-steps-table/variable-steps-table.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { ActionSheetController } from '@ionic/angular';
-import { StateStoreService } from 'src/app/state/state-store.service';
 import { VariableStep } from '../../models/variable-step.interface';
 
 @Component({
@@ -15,51 +13,21 @@ export class VariableStepsTableComponent implements OnInit {
     'read',
     'assign'
   ];
+  variableSteps: VariableStep[] = [];
 
-  constructor(public actionSheetController: ActionSheetController, public state: StateStoreService) { }
+  constructor() { }
 
   ngOnInit() {}
 
   addStep() {
-    this.state.variableSteps.forEach(s => s.selected = false);
-    this.state.variableSteps.push({selected: true});
+    this.variableSteps.forEach(s => s.selected = false);
+    this.variableSteps.push({selected: true});
   }
 
   select(step: VariableStep) {
-    this.state.variableSteps.forEach(s => s.selected = false);
+    this.variableSteps.forEach(s => s.selected = false);
     step.selected = true;
   }
 
-  printPdf() {
-    setTimeout(() => window.print(), 300);
-  }
-
-  exportJson() {
-    const dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify({ traceType: "variable-steps", body: this.state.variableSteps}));
-    const downloadAnchorNode = document.createElement('a');
-    downloadAnchorNode.setAttribute("href",     dataStr);
-    downloadAnchorNode.setAttribute("download", "traceTable.json");
-    document.body.appendChild(downloadAnchorNode); // required for firefox
-    downloadAnchorNode.click();
-    downloadAnchorNode.remove();
-  }
-
-  async export() {
-    const actionSheet = await this.actionSheetController.create({
-      header: 'Export trace',
-      buttons: [{
-        text: 'Print to PDF',
-        handler: () => {
-          this.printPdf();
-        }
-      }, {
-        text: 'Export as JSON',
-        handler: () => {
-          this.exportJson();
-        }
-      }]
-    });
-    await actionSheet.present();
-  }
 
 }
--- src/app/lenses/trace/trace.module.ts
+++ src/app/lenses/trace/trace.module.ts
@@ -5,11 +5,12 @@ import { MonacoEditorModule } from 'ngx-monaco-editor';
 import { FormsModule } from '@angular/forms';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
+import {MatRadioModule} from '@angular/material/radio';
 import { MatButtonModule } from '@angular/material/button';
 import { StudentTraceTablesComponent } from './components/student-trace-tables/student-trace-tables.component';
 import {MatTabsModule} from '@angular/material/tabs';
 import { TableTracerComponent } from './components/table-tracer/components/table-tracer/table-tracer.component';
-// import { VariableStepsTableComponent } from './components/variable-steps/components/variable-steps-table/variable-steps-table.component';
+import { VariableStepsTableComponent } from './components/variable-steps/components/variable-steps-table/variable-steps-table.component';
 // import { VariableValuesPageComponent } from './components/variable-values/components/variable-values-page/variable-values-page.component';
 import {MatSelectModule} from '@angular/material/select';
 @NgModule({
@@ -17,7 +18,7 @@ import {MatSelectModule} from '@angular/material/select';
     TraceLensComponent,
     StudentTraceTablesComponent,
     TableTracerComponent,
-    // VariableStepsTableComponent,
+    VariableStepsTableComponent,
     // VariableValuesPageComponent
   ],
   imports: [
@@ -28,7 +29,8 @@ import {MatSelectModule} from '@angular/material/select';
     MatCardModule,
     MatIconModule,
     MatButtonModule,
-    MatTabsModule
+    MatTabsModule,
+    MatRadioModule
   ],
   exports: [
     TraceLensComponent
