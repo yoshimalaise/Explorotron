2c918b51083ebfae219662d099d8cbadceefeb80
yoshimalaise <yoshimalaise@users.noreply.github.com>
2023-11-07 14:44:02 +0100

    added mircomaterials

--- src/app/components/micromaterial-list/bl/micromateriallist.ts
+++ src/app/components/micromaterial-list/bl/micromateriallist.ts
@@ -16,6 +16,40 @@ export function loadMicromaterials(): MicroMaterial[] {
             url: 'https://in-tech-gration.github.io/WDX-180/resources/css/selectors-explained/index.html',
             tags: ['css', 'css selectors'],
             github: 'https://github.com/KittyGiraudel/selectors-explained'
+        },
+        {
+            name: "JavaScript Array Explorer",
+            description: "Find the array method you need without digging through the docs.",
+            url: "https://arrayexplorer.netlify.app",
+            tags: ['javascript', 'arrays'],
+            github: "https://github.com/sdras/array-explorer"
+        },
+        {
+            name: "JavaScript Object Explorer",
+            description: "Find the object method you need without digging through the docs.",
+            url: "https://objectexplorer.netlify.app",
+            tags: ['javascript', 'objects', 'datastructures'],
+            github: "https://github.com/sdras/object-explorer"
+        },
+        {
+            name: 'Cron Trigger',
+            description: 'This is a micromaterial to practice both reading and writing cron expressions. There are cron reading exercises in which you need to select the right schedule matching the cron and there are writing exercises in which a cron needs to be constructed by rearranging blocks.',
+            url: 'https://cron-trigger.netlify.app',
+            tags: ['cron', 'linux'],
+            github: 'https://github.com/lpmi-13/cron-trigger'
+        },
+        {
+            name: 'Semantic Versioning Questions',
+            description: 'This is a micromaterial to practice reading and understanding semantic versioning (semver). It\'s based on the questions template by the amazing Julia Evans.',
+            url: 'https://semver-questions.netlify.app',
+            tags: ['versioning'],
+            github: 'https://github.com/lpmi-13/semver-questions'
+        },
+        {
+            name: 'SQL Murder Mystery',
+            description: `There's been a Murder in SQL City! The SQL Murder Mystery is designed to be both a self-directed lesson to learn SQL concepts and commands and a fun game for experienced SQL users to solve an intriguing crime.`,
+            url: 'https://mystery.knightlab.com',
+            tags: ['SQL']
         }
     ];
 
